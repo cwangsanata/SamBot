d9691261b4ab52a7dcc804ac512a9c56e3b05955
Chris Wangsanata <chriswangsanata@gmail.com>
2021-03-18 14:03:15 -0700

    So keep working on this you twat

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ async def ping(ctx):
     """
     await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
 
-
+@bot.command()
 async def shutdown(ctx):
     id = str(ctx.author.id)
     if id == 'your_id_here':
@@ -156,7 +156,7 @@ async def shutdown(ctx):
     else:
         await ctx.send("You don't have sufficient permissions to perform this action!")
 
-
+@bot.command()
 async def restart(ctx):
     id = str(ctx.author.id)
     if id == 'your_id_here':
